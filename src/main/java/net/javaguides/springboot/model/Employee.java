c1d6bee6f6c1ae4addb1c22c3954628925e43275
Zahaa Mac <Zahaawii@gmail.com>
2025-03-02 13:40:24 +0100

    initizaling project

---
+++ src/main/java/net/javaguides/springboot/model/Employee.java
@@ -0,0 +1,44 @@
+package net.javaguides.springboot.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="employees")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name ="first_name")
+    private String firstName;
+
+    @Column(name ="last_name")
+    private String lastName;
+
+    @Column(name ="email_id")
+    private String emailId;
+
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
